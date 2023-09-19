c1638807425a158a88a3c9de9e24e2eaa5710eed
aden anteng <hey@anteng.co>
2023-09-19 09:34:19 +0700

    atlantis

--- public/build/assets/ApplicationLogo.94e3f944.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as r,e as t,f as a}from"./app.db06b18a.js";const n={},s=["src"];function p(e,c){return e.$page.props.appSetting!=null?(r(),t("img",{key:0,src:e.$page.props.appSetting.media[0].original_url,class:"h-20 w-auto"},null,8,s)):a("",!0)}const m=o(n,[["render",p]]);export{m as A};

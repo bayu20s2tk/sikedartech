e0624933b4de1f90ddbd03542200c28866a6c9e5
Bayu Hanif Pratama <87808009+bayu20s2tk@users.noreply.github.com>
2023-11-15 09:25:14 +0000

    commit 4

--- resources/js/Pages/Course/Show.vue
+++ resources/js/Pages/Course/Show.vue
@@ -167,6 +167,7 @@ const createAssigment = () => {
                         <button class=" w-1/2 py-4 px-1 text-center border-b-2 font-medium text-sm"
                                 :class="tabCourseShow==3 ? 'border-cyan-600 text-cyan-600' : 'text-gray-500 border-gray-300 ' "
                                 @click="tabCourseShow=3"
+                                v-if="$page.props.user.role_id==1"
                         >
                             Lesson
                         </button>

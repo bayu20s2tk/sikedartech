2164b5560ff05bb0a7a3a6a56029cc2386809da4
Bayu Hanif Pratama <87808009+bayu20s2tk@users.noreply.github.com>
2023-11-16 04:45:00 +0000

    1

--- resources/js/Pages/Course/Show.vue
+++ resources/js/Pages/Course/Show.vue
@@ -170,7 +170,7 @@ const createAssigment = () => {
                                 
                         >
                         
-                            Lesson
+                            Lesson-
                         </button>
                         </template>
 
